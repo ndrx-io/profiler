d1f1b19d6178f130b3f056e84e1a87c48c3358c9
LAHAXE Arnaud <lahaxe.arnaud@gmail.com>
2015-11-07 17:40:49 +0100

    Add abstract config collector

---
+++ src/Collectors/Data/Config.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: arnaud
+ * Date: 01/11/15
+ * Time: 20:43
+ */
+
+namespace Ndrx\Profiler\Collectors\Data;
+
+use Ndrx\Profiler\Collectors\Collector;
+use Ndrx\Profiler\Collectors\Contracts\StartCollectorInterface;
+
+class PhpVersion extends Collector implements StartCollectorInterface
+{
+    /**
+     * Fetch data
+     * @return mixed
+     */
+    public function resolve()
+    {
+        $this->data = phpversion();
+    }
+
+    /**
+     * The path in the final json
+     * @example
+     *  path /aa/bb
+     *  will be transformed to
+     *  {
+     *     aa : {
+     *              bb: <VALUE OF RESOLVE>
+     *       }
+     *  }
+     * @return string
+     */
+    public function getPath()
+    {
+        return 'php-version';
+    }
+}
