f6b108e12288fe2efff519743f213c66ce6b92fc
LAHAXE Arnaud <lahaxe.arnaud@gmail.com>
2015-11-09 21:42:38 +0100

    #28 Cache abstract data collector

---
+++ src/Collectors/Data/Cache.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * User: LAHAXE Arnaud
+ * Date: 05/11/2015
+ * Time: 12:40
+ * FileName : User.php
+ * Project : profiler
+ */
+
+namespace Ndrx\Profiler\Collectors\Data;
+
+use Ndrx\Profiler\Collectors\Contracts\StreamCollectorInterface;
+use Ndrx\Profiler\Collectors\StreamCollector;
+
+abstract class Cache extends StreamCollector implements StreamCollectorInterface
+{
+    /**
+     * The path in the final json
+     * @example
+     *  path /aa/bb
+     *  will be transformed to
+     *  {
+     *     aa : {
+     *              bb: <VALUE OF RESOLVE>
+     *       }
+     *  }
+     * @return mixed
+     */
+    public function getPath()
+    {
+        return 'cache';
+    }
+}
