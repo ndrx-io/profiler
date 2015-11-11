83d6c8bab1d935f95aa34f950a7b389603562ec2
LAHAXE Arnaud <lahaxe.arnaud@gmail.com>
2015-11-11 20:29:39 +0100

    #46 Add Routes abstract collector

---
+++ src/Collectors/Data/Route.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * User: LAHAXE Arnaud
+ * Date: 05/11/2015
+ * Time: 12:40
+ * FileName : User.php
+ * Project : profiler
+ */
+
+namespace Ndrx\Profiler\Collectors\Data;
+
+use Ndrx\Profiler\Collectors\Contracts\StreamCollectorInterface;
+use Ndrx\Profiler\Collectors\StreamCollector;
+use Ndrx\Profiler\JsonPatch;
+
+abstract class Cache extends StreamCollector implements StreamCollectorInterface
+{
+    /**
+     * Write data in the datasource and clean current buffer
+     * @return mixed
+     */
+    public function stream()
+    {
+        $patch = $this->jsonPatch->generate($this->getPath(), JsonPatch::ACTION_ADD, $this->data, true);
+        $this->dataSource->save($this->process, [$patch]);
+        $this->data = [];
+    }
+
+    /**
+     * The path in the final json
+     * @example
+     *  path /aa/bb
+     *  will be transformed to
+     *  {
+     *     aa : {
+     *              bb: <VALUE OF RESOLVE>
+     *       }
+     *  }
+     * @return mixed
+     */
+    public function getPath()
+    {
+        return 'cache';
+    }
+}
